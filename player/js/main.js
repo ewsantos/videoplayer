00e5a392fdf5a8c0437e9bf7b7710c50ba709bd8
ewsantos <22549387+ewsantos@users.noreply.github.com>
2019-04-09 19:26:44 -0700

    commit'

--- player/js/main.js
+++ player/js/main.js
@@ -5,26 +5,14 @@ $(document).ready(function(){
 
   var video = new Video("../videos/vid1.mp4", true);
 
-  video.height = 280;
-  video.width = 500;
+  $("video").attr('id', 'video');
 
-    video.play();
+  //  video.play();
 
-/*  $("#player").mousedown(function(event) {
-    switch (event.which) {
-        case 1:
-            alert('Left Mouse button pressed.');
-            break;
-        case 2:
-            alert('Middle Mouse button pressed.');
-            break;
-        case 3:
-            alert('Right Mouse button pressed.');
-            break;
-        default:
-            alert('You have a strange Mouse!');
-    }
-});*/
+    $('#video').bind("contextmenu",function(e){
+       video.play();
+       return false;
+    });
 
 });
 
